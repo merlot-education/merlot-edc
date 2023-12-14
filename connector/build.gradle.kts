a44722c2d7ca551927cb7c2a730dc54cc247c86e
M-Busk <125284318+M-Busk@users.noreply.github.com>
2023-12-14 11:31:20 +0100

    Ionos extension maven (#2)
    * switch to maven build
    
    * update ionos extension version

--- connector/build.gradle.kts
+++ connector/build.gradle.kts
@@ -20,14 +20,19 @@ plugins {
 
 repositories {
     mavenLocal()
-	mavenCentral()
-    maven {// while runtime-metamodel dependency is still a snapshot
-		url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+    mavenCentral()
+    maven {
+        url = uri("https://maven.pkg.github.com/Digital-Ecosystems/edc-ionos-s3")
+        credentials {
+            username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+            password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+        }
     }
 }
 
 val group: String by project
 val edcVersion: String by project
+val ionosS3Version: String by project
 
 dependencies {
     implementation("${group}:dsp:${edcVersion}")
@@ -48,13 +53,13 @@ dependencies {
 	implementation("${group}:policy-model:${edcVersion}")		
 	implementation("${group}:contract-spi:${edcVersion}")	
 
-	implementation(project(":edc-ionos-extension:provision-ionos-s3"))
+	implementation("com.ionoscloud.edc:provision-ionos-s3:${ionosS3Version}")
 
     // provider
     implementation("${group}:transfer-data-plane:${edcVersion}")
     implementation("${group}:data-plane-core:${edcVersion}")
     implementation("${group}:data-plane-client:${edcVersion}")
-    implementation(project(":edc-ionos-extension:data-plane-ionos-s3"))
+    implementation("com.ionoscloud.edc:data-plane-ionos-s3:${ionosS3Version}")
 }
 
 application {
